3f1dc6bff264613c91482102bba2a8ac757d5b94
Victor Ruelas-Rivera <ruelasvh@gmail.com>
2017-09-01 19:40:23 -0400

    Cleaning up

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -12,11 +12,9 @@ class AuthenticateUser
 
   private
 
-  attr_accessor :email, :password
-
   def user
-    user = User.find_by_email(email)
-    return user if user && user.authenticate(password)
+    user = User.find_by_email(@email)
+    return user if user && user.authenticate(@password)
     errors.add :user_authentication, 'invalid credentials'
     nil
   end
